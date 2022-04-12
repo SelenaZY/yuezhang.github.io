d9ee298f0d75494d0ae0f43a050116df01ddc0b0
YueZ05 <zhyue05@gmail.com>
2022-04-11 23:44:18 -0400

    uodate

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@
                         scrollTop: target.offset().top,
                     },
                     1000,
-                    "easeInOutExpo"
+                    "easeInOutExpo" 
                 );
                 return false;
             }
